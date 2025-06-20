afcb006ca989f8ca6b036f328614c03a1b8e2a49
supri <unknown@student.adelaide.edu.au>
2025-06-20 21:00:10 +0000

    [automatic commit] 2025-06-20 21:00:09 UTC 129.127.202.95

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
   console.log('Received:', username, password); // for debug
 
   try {
-    //
+    //replaced email with username
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
