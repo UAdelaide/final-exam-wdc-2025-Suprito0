c181f6b301c6debddc69ab868cd1808fceffece9
supri <unknown@student.adelaide.edu.au>
2025-06-20 22:14:45 +0000

    [automatic commit] 2025-06-20 22:14:44 UTC 129.127.202.95

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -60,7 +60,7 @@ router.post('/:id/apply', async (req, res) => {
 });
 
 // GET dog_id and name
-router.get('/registered-dogs', async (req, res) =>{
+router.get('/registered-dogs', async (req, res) => {
   if(!req.session.user) {
     return res.status(401).json({error: 'Not logged in'});
   }
