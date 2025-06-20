a931f8e951f597837f44e34be9840cd38d24033e
supri <unknown@student.adelaide.edu.au>
2025-06-20 21:15:14 +0000

    [automatic commit] 2025-06-20 21:15:13 UTC 129.127.202.95

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -73,9 +73,9 @@ router.post('/login', async (req, res) => {
 
 // LOGOUT
 router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.error()
+  req.session.destroy(error => {
+    if (error) {
+      console.error('Logout error: ', error)
     }
   })
 });
