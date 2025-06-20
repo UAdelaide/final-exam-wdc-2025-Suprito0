231de367c71f33c336a70bf0e829d3a9f5d2dad6
supri <unknown@student.adelaide.edu.au>
2025-06-20 19:49:09 +0000

    [automatic commit] 2025-06-20 19:49:08 UTC 129.127.202.95

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,7 +52,7 @@ router.post('/login', async (req, res) => {
     const user = row[0];
 
     if(user.role === 'walker'){
-      return res.sendFile)
+      return res.redirect('/')
     }
 
     res.send({ message: 'Login successful', user: rows[0] });
