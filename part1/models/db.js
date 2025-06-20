2d003f4a50b82950483f07bf9b841c165751a272
Suprito <a1950138@adelaide.edu.au>
2025-06-20 18:07:57 +0000

    part 1

---
+++ part1/models/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'DogWalkService',
+    waitForConnections: true,
+    connectionLimit: true,
+    queueLimit: 0
+});
+
+module.exports = pool;
