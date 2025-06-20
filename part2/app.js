ee4fd5069e58586a0746de0bcbac76196a716069
supri <unknown@student.adelaide.edu.au>
2025-06-20 21:05:20 +0000

    [automatic commit] 2025-06-20 21:05:19 UTC 129.127.202.95

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 require('dotenv').config();
 
@@ -8,16 +7,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(session({
-    secret: 'key',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-        httpOnly: true
-    }
-}));
-app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
